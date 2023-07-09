f4141fdc1ebc11b3e58c89d721f8f57ddd4eb326
Lazaran <eq.scurr@gmail.com>
2023-07-09 15:17:22 -0300

    Update to auto-paste and auto-copy

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 var template = "https://drive.google.com/uc?id="
 
-document.querySelector('#drivelink').addEventListener('input', topUpdate);
-function topUpdate(e){
-    hashValue = e.target.value
-    if (hashValue != ""){
-        hash = hashValue.split("/")
-        embedlink = template + hash[hash.length-2]
-        console.log(embedlink)
+
+document.getElementById("drivelink").onclick = function(){
+    navigator.clipboard
+    .readText()
+    .then((clipText) => {
+        document.getElementById("drivelink").value = clipText
+        hashValue = clipText;
+        hash = hashValue.split("/");
+        embedlink = template + hash[hash.length-2];
+        console.log(embedlink);
         document.getElementById("embedlink").value = embedlink
-    }
+    });
+};
+
+document.getElementById("embedlink").onclick = function(){
+    navigator.clipboard.writeText(document.getElementById("embedlink").value)
 };
